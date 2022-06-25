fe4b479608f23a64297aa82045bd6bd7dc57a29b
Daniil Rassadin <daniil@rassadin.org>
2022-06-25 18:21:38 +0300

    Задание 3

---
+++ VK/VK/Friends/Photos/PhotosViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  PhotosViewCell.swift
+//  VK
+//
+//  Created by Daniil Rassadin on 25.06.2022.
+//
+
+import UIKit
+
+class PhotosViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var photo: UIImageView!
+    
+}
---
+++ VK/VK/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  VK
+//
+//  Created by Daniil Rassadin on 25.06.2022.
+//
+
+import UIKit
+
+struct User {
+    let name: String
+    let image: UIImage?
+}
